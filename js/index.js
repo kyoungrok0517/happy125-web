c011457f132c5a0525ad3d3163a8ac73a7ab4d50
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-02-03 19:44:13 +0900

    프로토타입 완성

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-var app = angular.module("app", ["firebase"]);
+angular.module("app", ["firebase"])
 
-app.controller("PostCtrl", function ($scope, $firebaseArray) {
-    var _postsRef = new Firebase("https://happy125.firebaseio.com/posts");
-    $scope.posts = $firebaseArray(_postsRef);
-    
-});
+    .controller("PostCtrl", function ($scope, $firebaseArray) {
+        var _postsRef = new Firebase("https://happy125.firebaseio.com/posts");
+        $scope.posts = $firebaseArray(_postsRef);
+    })
+
+    .directive('dynamicMoreButtonDirective', function () {
+        return function (scope, element, attrs) {
+            // componentHandler.upgradeElement(element[0]);
+            
+            // console.log(element[0]);
+        };
+    })
+
