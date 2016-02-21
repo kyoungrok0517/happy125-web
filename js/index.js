ef0c5ccf8bcb2985b90f40f540bcd563e2081fd7
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-02-21 13:35:52 +0900

    feat: 로그인 기능 구현중

--- js/index.js
+++ js/index.js
@@ -5,6 +5,25 @@ angular.module("app", ["firebase"])
         $scope.posts = $firebaseArray(_postsRef);
     })
 
+    .controller("LoginCtrl", function ($scope) {
+        var _ref = new Firebase("https://happy125.firebaseio.com");
+
+        $scope.login = function ($event) {
+            console.log($event);
+            
+            // _ref.authWithPassword({
+            //     email: email,
+            //     password: password
+            // }, function(error, authData) {
+            //     if (error) {
+            //         console.log("Error:", error);
+            //     } else {
+            //         console.table(authData)
+            //     }
+            // });
+        }
+    })
+
     .directive('dynamicMoreButtonDirective', function () {
         return function (scope, element, attrs) {
             // componentHandler.upgradeElement(element[0]);
