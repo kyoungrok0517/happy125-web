1ea5bccd9f5d70d184190ca82763fab516c93d4f
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-03-01 18:29:26 +0900

    refactor: ui-router 적용 직전

--- media/js/index.js
+++ media/js/index.js
@@ -15,62 +15,14 @@ angular.module("app", ["firebase", "ngStorage"])
         $scope.posts = PostSrv.posts;
     })
 
-    .controller("LoginCtrl", function ($scope, $window) {
-        var _ref = new Firebase("https://happy125.firebaseio.com");
-
-        $scope.loginWithFacebook = function () {
-            _ref.authWithOAuthRedirect("facebook", function (error, authData) {
-                if (error) {
-                    console.log("Login Failed!", error);
-                } else {
-                    console.log("Authenticated successfully with payload:", authData);
-                }
-            }, { 'scope': 'email,public_profile' });
-        }
-
-        $scope.loginWithEmail = function ($event) {
-            var email = $scope.user.email;
-            var password = $scope.user.password;
-
-            _ref.authWithPassword({
-                email: email,
-                password: password
-            }, function (error, authData) {
-                if (error) {
-                    console.log("Error:", error);
-                } else {
-                    console.table(authData);
-
-                    $scope.$storage.authData = authData;
-                }
-            });
-        }
-
-        $scope.logout = function () {
-            _ref.unauth();
-
-            $window.location.reload();
-        }
+    .controller("LoginCtrl", function ($scope, AuthSrv) {
+        $scope.loginWithFacebook = AuthSrv.loginWithFacebook;
+        $scope.loginWithEmail = AuthSrv.loginWithEmail;
+        $scope.logout = AuthSrv.logout;
     })
 
-    .controller("RegisterCtrl", function ($scope) {
-        var _ref = new Firebase("https://happy125.firebaseio.com");
-
-        $scope.register = function ($event) {
-            var email = $scope.user.email;
-            var password = $scope.user.password;
-
-            _ref.createUser({
-                email: email,
-                password: password
-            }, function (error, userData) {
-                if (error) {
-                    console.log("Error:", error);
-                } else {
-                    console.table(userData);
-                }
-            });
-        }
+    .controller("RegisterCtrl", function ($scope, AuthSrv) {
+        $scope.register = AuthSrv.registerWithEmail;
     })
 
     .factory("PostSrv", function ($log, $firebaseArray) {
@@ -81,15 +33,30 @@ angular.module("app", ["firebase", "ngStorage"])
             return -moment(post.id).unix();
         }
 
+        // Like object 가져오기
+        // _posts.$loaded().then(function () {
+        //     if (AuthSrv.isLoggedIn()) {
+        //         var uid = AuthSrv.getUid();
+        //         var likeObj = getMyLikeObject(uid);
+        //         likeObj.$loaded().then(function () {
+        //             angular.forEach(likeObj, function (value, key) {
+        //                 PostSrv._posts.$getRecord(key)._likedByMe = new Boolean(value);
+        //             })
+        //         });
+        //     }
+        // });
+
         // Production 코드에선 삭제
-        _postsRef.on("value", function (snapshot) {
-            snapshot.forEach(function (childSnapshot) {
-                var priority = getPriority(childSnapshot.val());
-                childSnapshot.ref().setPriority(priority);
-            });
+        // _postsRef.on("value", function (snapshot) {
+        //     snapshot.forEach(function (childSnapshot) {
+        //         var priority = getPriority(childSnapshot.val());
+        //         childSnapshot.ref().setPriority(priority);
+        //     });
+
             
-            // componentHandler.upgradeAllRegistered();
-        });
+            
+        //     // componentHandler.upgradeAllRegistered();
+        // });
 
         return {
             posts: _posts,
@@ -117,20 +84,38 @@ angular.module("app", ["firebase", "ngStorage"])
             postUrl = "https://happy125.firebaseio.com/posts",
             postRef = new Firebase(postUrl);
 
-        var likeSnapshot = {};
-        likeRef.on("value", function (snapshot) {
-            likeSnapshot = snapshot;
-        });
-
         // functions
+        function like(post, uid) {
+            var path = getLikePath(post, uid);
+            if (!this.isLiked(post, uid)) {
+                likeRef.child(path).set(true);
+                uplike(post);
+            }
+
+            $log.debug("Liked");
+        }
+
+        function unlike(post, uid) {
+            var path = getLikePath(post, uid);
+            if (this.isLiked(post, uid)) {
+                likeRef.child(path).remove();
+                downlike(post);
+            }
+
+            $log.debug("Unliked");
+        }
+
         function uplike(post) {
-            var countRef = new Firebase(postUrl + "/" + post.$id + "/likes");
+            var path = getLikeCountPath(post);
+            var countRef = new Firebase(path);
             countRef.transaction(function (current_value) {
                 return (current_value || 0) + 1;
             });
         }
+
         function downlike(post) {
-            var countRef = new Firebase(postUrl + "/" + post.$id + "/likes");
+            var path = getLikeCountPath(post);
+            var countRef = new Firebase(path);
             countRef.transaction(function (current_value) {
                 var _current_value = current_value || 0;
                 if (_current_value <= 0) {
@@ -140,40 +125,78 @@ angular.module("app", ["firebase", "ngStorage"])
                 }
             });
         }
-        function getPath(post, uid) {
-            return post.$id + "/" + uid;
+
+        function getLikePath(post, uid) {
+            return uid + "/" + post.$id;
+        }
+
+        function getLikeCountPath(post) {
+            return postUrl + "/" + post.$id + "/likes";
+        }
+
+        function getMyLikeObject(uid) {
+            return $firebaseObject(likeRef.child(uid));
         }
 
         return {
-            like: function (post, uid) {
-                var path = getPath(post, uid);
-                if (!this.isLiked(post, uid)) {
-                    likeRef.child(path).set(true);
-                    uplike(post);
-                }
-            },
-            unlike: function (post, uid) {
-                var path = getPath(post, uid);
-                if (this.isLiked(post, uid)) {
-                    likeRef.child(path).remove();
-                    downlike(post);
-                }
-            },
-            isLiked: function (post, uid) {
-                var path = getPath(post, uid);
-                return likeSnapshot.child(path).exists();
-            }
+            like: like,
+            unlike: unlike,
+            getMyLikeObject: getMyLikeObject
         };
     })
 
-    .factory("AuthSrv", function ($log, $localStorage, $rootScope) {
+    .factory("AuthSrv", function ($log, $localStorage, $rootScope, $window, $firebaseAuth) {
         // TODO: Auth.$requireAuth();
         
         var _ref = new Firebase("https://happy125.firebaseio.com");
         var _authData = _ref.getAuth();
 
+        function loginWithFacebook() {
+            _ref.authWithOAuthRedirect("facebook", function (error, authData) {
+                if (error) {
+                    console.log("Login Failed!", error);
+                } else {
+                    console.log("Authenticated successfully with payload:", authData);
+                }
+            }, { 'scope': 'email,public_profile' });
+        }
+
+        function loginWithEmail(email, password) {
+            _ref.authWithPassword({
+                email: email,
+                password: password
+            }, function (error, authData) {
+                if (error) {
+                    console.log("Error:", error);
+                } else {
+                    console.table(authData);
+                }
+            });
+        }
+
+        function logout() {
+            _ref.unauth();
+            $window.location.reload();
+        }
+
+        function registerWithEmail(email, password) {
+            _ref.createUser({
+                email: email,
+                password: password
+            }, function (error, userData) {
+                if (error) {
+                    console.log("Error:", error);
+                } else {
+                    console.table(userData);
+                }
+            });
+        }
+
         return {
-            ref: _ref,
+            loginWithFacebook: loginWithFacebook,
+            loginWithEmail: loginWithEmail,
+            logout: logout,
+            registerWithEmail: registerWithEmail,
             authData: _authData,
             isLoggedIn: function () {
                 return this.authData;
@@ -255,8 +278,13 @@ angular.module("app", ["firebase", "ngStorage"])
                     // enable & init like button
                     var likeButton = angular.element(element.find('button')[1]);
                     likeButton.removeAttr('disabled')
+                    // var isLiked = LikeSrv.isLiked(post, uid);
+                    var likedByMe = post._likedByMe;
+                    if (likedByMe) {
+                        likeButton.addClass('mdl-color-text--pink');
+                    }
                     likeButton.on('click', function (event) {
-                        if (LikeSrv.isLiked(post, uid)) {
+                        if (likedByMe) {
                             LikeSrv.unlike(post, uid);
                             likeButton.removeClass('mdl-color-text--pink');
                         } else {
